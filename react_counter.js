e04bcbe5dd120f3d35eeb0d0dcecdcaadef3bc64
VarshUI <145260709+VarshUI@users.noreply.github.com>
2024-11-05 16:57:03 +0530

    Add files via upload

---
+++ react_counter.js
@@ -0,0 +1,22 @@
+import { useState } from "react"; //allow us to use states
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+function App() {
+  const [counter, setCounter] = useState(0); //using object destructuring
+  //whenever there is a "use" as prefix then it's called a hook
+
+  return (
+    <div className="App">
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
+    </div>
+  );
+}
+
+export default App;
