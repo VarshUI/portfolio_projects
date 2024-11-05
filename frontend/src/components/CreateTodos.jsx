fde9639bcc1864c157546a8a3252b447db1dfa6f
VarshUI <145260709+VarshUI@users.noreply.github.com>
2024-11-05 13:30:59 +0530

    Delete frontend directory

--- frontend/src/components/CreateTodos.jsx
+++
@@ -1,9 +0,0 @@
-export function CreateTodo() {
-  return (
-    <div>
-      <input type="text" placeholder="title"></input> <br />
-      <input type="text" placeholder="description"></input> <br />
-      <button>Add a todo</button>
-    </div>
-  );
-}
--- frontend/src/components/Todos.jsx
+++
@@ -1,9 +0,0 @@
-export function Todos() {
-  return (
-    <div>
-      <h1>Go to gym</h1>
-      <h2>You need to go to the gym</h2>
-      <button>Mark as completed</button>
-    </div>
-  );
-}
